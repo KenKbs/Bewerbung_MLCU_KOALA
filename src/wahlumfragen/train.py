5564d37eb190e59881211e6f5052d75aebeb80dd
KenKbs <kenny.kubsch@gmail.com>
2026-05-19 18:08:32 +0200

    Project initialization

---
+++ src/wahlumfragen/train.py
@@ -0,0 +1,10 @@
+from wahlumfragen.model import Model
+from wahlumfragen.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
